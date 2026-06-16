37621a359eab6b50dcf239cef087b027edb942de
antonin.p <antonin.p@lahautesociete.com>
2026-06-16 16:16:16 +0200

    feat: add ParsedChapter and ParsedSection DTOs and configure League\CommonMark

---
+++ site/src/Dto/ParsedSection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Dto;
+
+use App\Enum\SectionType;
+
+final readonly class ParsedSection
+{
+    public function __construct(
+        public SectionType $type,
+        public string $title,
+        public string $html,
+        public int $position,
+    ){
+
+    }
+
+}
